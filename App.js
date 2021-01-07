4108e67bf5c57b6692a93a5bb06dc6f6acf2c7ac
Jieying Yang <jieying.yang@cca.edu>
2021-01-06 20:03:32 -0800

    material

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <Container>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Container = styled.View`
+
+`;
