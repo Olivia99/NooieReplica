e448804eb5de634cffe57809940270a86406cfbd
Jieying Yang <jieying.yang@cca.edu>
2021-01-21 15:27:34 -0800

    created base station page

---
+++ .expo-shared/screens/home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import styled from "styled-components";
+
+class HomeScreen extends React.Component {
+    render() {
+        return (
+
+            <Container>
+                <Text>
+                    this is a text
+                </Text>
+            </Container>
+        )
+    }
+}
+
+const Container = styled.View``
+const Text = styled.Text
